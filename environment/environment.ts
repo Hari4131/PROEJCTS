ae3fcf45402f60bc7921d304c88d007ecdfadd50
Hari4131 <rematahari143@gmail.com>
2025-12-16 13:16:05 +0530

    Add files via upload

---
+++ environment/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+
+  // staging API url
+  // API_Base_URL:'https://telemedicinepvtapi.esdinfra.com/api/',
+  // Login_URL:'https://telemedicinepvtapi.esdinfra.com/'
+
+  ////TestingApisupport
+  //API_Base_URL:'https://demo.pvtapi.esdinfra.com/api/',
+ // Login_URL:'https://demo.pvtapi.esdinfra.com/'
+  
+  //// live API url
+   API_Base_URL:'https://api.aviationacademy.co.in/api/',
+   Login_URL: 'https://portal.aviationacademy.co.in/#/auth/login'
+  };
